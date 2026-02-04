022b343a29b2361910bcd3e2ac9f7ec5dc78cd61
rsayhii <rsayhii001@gmail.com>
2026-02-04 10:44:28 +0530

    comment

--- resources/views/admin/calender.blade.php
+++ resources/views/admin/calender.blade.php
@@ -1,6 +1,6 @@
 @extends('components.layout')
 @section('content')
-
+{{-- Calendar Container --}}
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <meta name="csrf-token" content="{{ csrf_token() }}">
